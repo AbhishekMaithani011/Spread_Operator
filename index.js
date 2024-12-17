9c664d8f3b09ccb9bcf6d4c811fe3945db79f87c
AbhishekMaithani011 <109787856+AbhishekMaithani011@users.noreply.github.com>
2024-12-17 22:53:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["Lemon", "Lime", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "coconut"];
+
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "Monkey D",
+//   lName: "Luffy",
+// };
+// const User = {
+//   ...fullName,
+//   id: 1,
+//   userName: "Monkey'D_Luffy001",
+// };
+
+// console.log(User);
